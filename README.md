9f2a693c57417d08af7298a88a2c4c33df9ee4b4
wonni <lsw8168.naver.com>
2016-08-22 16:55:34 +0900

    add all

---
+++ README.md
@@ -0,0 +1,4 @@
+# webpack_test
+
+실행<br>
+webpack-dev-server --host 0.0.0.0 --port 7777
---
+++ entry.js
@@ -0,0 +1,6 @@
+import './style.sass';
+
+import hello from './hello';
+import world from './world';
+
+document.write(`${hello}, ${world}!`);
